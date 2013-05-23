c6e7714325a36d0c12047b7ff2204a4bfe4085b8
Robert Unnthorsson <09robert.u@gmail.com>
2013-05-23 12:28:19 +0200

    test

--- source/ThermoMash/src/thermomash/ThermoMash.java
+++ source/ThermoMash/src/thermomash/ThermoMash.java
@@ -14,6 +14,6 @@ public class ThermoMash {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Test");
+        System.out.print("Test1");
     }
 }
