2b18dec5869006af50d2dffd82d0555f4822f814
BogdanSorlea <bogdan.sorlea@gmail.com>
2013-05-23 12:21:00 +0200

    Added Netbeans project.

---
+++ source/ThermoMash/src/thermomash/ThermoMash.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thermomash;
+
+/**
+ *
+ * @author Bogdan
+ */
+public class ThermoMash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Test");
+    }
+}
