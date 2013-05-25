55cb9a0a0863196ac26f4cdade0ec4d89d3c0f91
BogdanSorlea <bogdan.sorlea@gmail.com>
2013-05-25 13:58:17 +0200

    Committed temporary application.

---
+++ source/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,250 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Bogdan
+ */
+public class JavaApplication2 {
+
+    //private String lastResponseIP;
+    
+    private static boolean IS_ADMIN = true;
+    private static boolean IS_WORKER = false;
+    private static boolean IS_MONITOR = false;
+    private static int noOfNodes = 0;
+    private static String lastResponseIP = null;
+    private static String adminIP = null;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ServerSocket server = null;
+        Socket clientSocket = null;
+        
+        try {
+            server = new ServerSocket(Settings.TCP_PORT);
+        } catch (IOException ex) {
+            Logger.getLogger(JavaApplication2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        for (int i=0; i<5; i++){
+            
+            transmitBroadcast(Settings.NETWORK_ATTACH_REQ);
+            String response = null;
+            
+            //response = receiveBroadcast(Settings.BROADCAST_RECEIVE_TIMEOUT);
+            
+            try {
+                server.setSoTimeout(Settings.TCP_RECEIVE_TIMEOUT);
+                clientSocket = server.accept();
+                BufferedReader is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                //PrintStream os = new PrintStream(clientSocket.getOutputStream());
+                response = is.readLine();
+                is.close();
+                clientSocket.close();
+            } catch (IOException ex) {
+                Logger.getLogger(JavaApplication2.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            if ( response != null
+                    && response.contains(Settings.NETWORK_ATTACH_CONFIRM) ) {
+                IS_WORKER = true;
+                IS_ADMIN = false;
+                adminIP = lastResponseIP.trim().replace("/", "")
+                        .replace(":" + Settings.BROADCAST_PORT, "");
+                break;
+            }
+            if ( response != null
+                    && response.contains(Settings.NETWORK_ATTACH_MONITOR) ) {
+                IS_WORKER = true;
+                IS_ADMIN = false;
+                IS_MONITOR = true;
+                adminIP = lastResponseIP.trim().replace("/", "")
+                        .replace(":" + Settings.BROADCAST_PORT, "");;
+                break;
+            }
+        }
+        
+        if (IS_WORKER)
+            System.out.println("IS WORKER!");
+        else if (IS_ADMIN)
+            System.out.println("IS ADMIN!");
+        else if (IS_MONITOR)
+            System.out.print("IS MONITOR!");
+        
+        if (IS_ADMIN)
+            System.out.print("  MY IP IS: " + getIP());
+        else
+            System.out.print("  ADMIN IP IS: " + adminIP);
+        
+//        ServerSocket servsocket = null;
+//        Socket socket = null;
+        
+        if (IS_ADMIN) {
+            noOfNodes = 1;
+//            try {
+//                servsocket = new ServerSocket(Settings.TCP_PORT);
+//            } catch (IOException ex) {
+//                System.exit(-1);
+//            }
+            
+        }
+        
+        while(true){
+            
+            String request = null;
+            request = receiveBroadcast(Settings.BROADCAST_FAST_RECEIVE_TIMEOUT);
+            
+            if (IS_ADMIN) {
+                if (request != null){
+                    if (request.equals(Settings.NETWORK_ATTACH_REQ)){
+                        String data = null;
+                        if (noOfNodes == 1)
+                            //transmitBroadcast(Settings.NETWORK_ATTACH_CONFIRM);
+                            data = Settings.NETWORK_ATTACH_CONFIRM;
+                        else
+                            //transmitBroadcast(Settings.NETWORK_ATTACH_MONITOR);
+                            data = Settings.NETWORK_ATTACH_MONITOR;
+                        
+                        clientSocket = null;
+                        try {
+                            clientSocket = new Socket(lastResponseIP, Settings.TCP_PORT);
+                            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                            out.write(data);
+                        } catch (UnknownHostException ex) {
+                            Logger.getLogger(JavaApplication2.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (IOException ex) {
+                            Logger.getLogger(JavaApplication2.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        
+                        
+                        
+                        noOfNodes++;
+                    }                
+                }
+                
+//                String received = null;
+//                try {
+//                    socket = servsocket.accept();
+//                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                    
+//                    while (!in.ready()){}
+//                    received = in.readLine();
+//                    System.out.print(received);
+//                    //in.close();
+//                    //socket.close();
+//                } catch (IOException ex) {
+//                    System.exit(-2);
+//                }
+            }
+            
+            if (IS_WORKER) {
+                if (request != null){
+
+                }
+                
+//                try {
+//                    Socket clientsocket = new Socket(adminIP, Settings.TCP_PORT);
+//                    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientsocket.getOutputStream()));
+//                    out.write("DATA:" + Long.toString(20 + Math.round(Math.random() * 10)));
+//                    out.close();
+//                    clientsocket.close();
+//                } catch (UnknownHostException ex) {
+//                    System.exit(-3);
+//                } catch (IOException ex) {
+//                    System.exit(-4);
+//                }
+                
+            }
+                
+            
+        }
+        
+    }
+    
+    
+    
+    private static void transmitBroadcast(String data){
+        try {
+            MulticastSocket socket = new MulticastSocket(Settings.BROADCAST_PORT);
+
+            byte[] buffer = data.getBytes();
+
+            InetAddress group = InetAddress.getByName(Settings.BROADCAST_ADDRESS);
+            DatagramPacket packet = new DatagramPacket(buffer,
+                    buffer.length,
+                    group,
+                    Settings.BROADCAST_PORT);
+            socket.send(packet);
+            socket.close();
+        } catch (IOException e){
+        }
+    }
+    
+    private static String receiveBroadcast(int timeout){
+        String data = null;
+        try {
+            MulticastSocket socket = new MulticastSocket(Settings.BROADCAST_PORT);
+            InetAddress group = InetAddress.getByName(Settings.BROADCAST_ADDRESS);
+            socket.joinGroup(group);
+            socket.setSoTimeout(timeout);
+            DatagramPacket packet;
+            byte[] buf = new byte[256];
+            packet = new DatagramPacket(buf, buf.length);
+            
+            try {
+                socket.receive(packet);
+                data = new String(packet.getData());
+                data = data.trim();
+               
+                lastResponseIP = packet.getSocketAddress().toString();//socket.getRemoteSocketAddress().toString();
+                //System.out.println("broadcast from: " + lastResponseIP);
+            } catch (SocketTimeoutException e) {
+                data = null;
+                lastResponseIP = null;
+            }
+            
+            socket.leaveGroup(group);
+            socket.close();
+        } catch (UnknownHostException ex) {
+            //Logger.getLogger(MulticastClientThread.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            //Logger.getLogger(MulticastClientThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return data;
+    }
+    
+    private static String getIP(){
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException ex) {
+        }
+        return null;
+    }
+}
---
+++ source/JavaApplication2/src/javaapplication2/Settings.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Bogdan
+ */
+public class Settings {
+    
+    public static String BROADCAST_ADDRESS = "225.1.1.1";//10.16.0.0";//192.168.1.0";
+    public static int BROADCAST_PORT = 4446;
+    public static int TCP_PORT = 4447;
+    
+    static String NETWORK_ATTACH_REQ = "NATTREQ";
+    static int BROADCAST_RECEIVE_TIMEOUT = 300; // in miliseconds
+    static String NETWORK_ATTACH_CONFIRM = "NATTCONF";
+    static int BROADCAST_FAST_RECEIVE_TIMEOUT = 500;
+    static String NETWORK_ATTACH_MONITOR = "NATTMONIT";
+    static int TCP_RECEIVE_TIMEOUT = 300;
+    
+    
+    
+}
